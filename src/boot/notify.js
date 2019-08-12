96530b6fbd27b5a4564cb2ef639b170656b43edb
stokaboka <igothorev@gmail.com>
2019-08-12 19:56:01 +0300

    feat: notify delay

--- src/boot/notify.js
+++ src/boot/notify.js
@@ -2,7 +2,7 @@ import { Notify } from 'quasar'
 
 Notify.setDefaults({
   position: 'bottom-right',
-  timeout: 1000,
+  timeout: 2000,
   textColor: 'white',
   actions: [{ icon: 'close', color: 'white' }]
 })
